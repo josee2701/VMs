c83dfd38edbf825e9671dcbe07ea5f825cdde53d
Jose Campos <desarrollador8@azsmart.net>
2025-04-22 09:38:06 -0500

    feat: Agregar enumeración de roles para el modelo de usuario

--- models/model.py
+++ models/model.py
@@ -1,15 +1,24 @@
 
+from enum import Enum
 from typing import Optional
 
 from pydantic import EmailStr
 from sqlmodel import Field, SQLModel
 
 
+class RoleEnum(str, Enum):
+    administrator = "administrator"
+    client        = "cliente"
+
 class UserBase(SQLModel):
     name: str = Field(..., max_length=100)
     password: str = Field(..., max_length=100)
     email: EmailStr
-
+    rol: RoleEnum = Field(default=RoleEnum.client)
+    
+    def __str__(self):
+        return f"{self.name} - {self.email} - {self.rol}"
+    
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
