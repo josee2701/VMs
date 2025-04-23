346f8a9f7bbfe894cd09aed461427dcc2708c630
Jose Campos <j.camposs2701@gmail.com>
2025-04-23 08:51:05 -0500

    feat: Añadir middleware CORS para permitir solicitudes desde orígenes específicos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from configs.db import create_db_and_tables
 from models.users import *
@@ -8,7 +9,19 @@ from routers.user import router as user_router
 from routers.vm import router as vm_router
 
 app = FastAPI()
+# Orígenes que permites
+origins = [
+    "http://localhost:5173",
+]
 
+# Añade el middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # <- aquí tus orígenes
+    allow_credentials=True,           # si usas cookies/autenticación basada en credenciales
+    allow_methods=["*"],              # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],              # Authorization, Content-Type, …
+)
 # Incluimos el router de users
 app.include_router(user_router)
 # Incluimos el router de auth
